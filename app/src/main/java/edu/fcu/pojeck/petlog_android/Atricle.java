f13e2dab5ec6c9a1332885c69a68f3fabee28dc8
sasalicat <waimaoisme@126.com>
2016-05-14 18:55:46 +0800

    the class use to keep the atricle user send

---
+++ app/src/main/java/edu/fcu/pojeck/petlog_android/Atricle.java
@@ -0,0 +1,14 @@
+package edu.fcu.pojeck.petlog_android;
+
+import java.io.File;
+
+
+public class Atricle {//the data struct of article
+    private File[] icon;
+    private String word;
+    private int ownerid;
+    private int id;//the id of itself
+    private int likeid;//match the like  this  atricle gets in like database
+    private int messageid;//match the leavemessage in leavemessage database
+    private String date;
+}
