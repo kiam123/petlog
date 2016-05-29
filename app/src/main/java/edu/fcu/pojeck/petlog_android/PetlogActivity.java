cbde2bc1e564fe8d8c1ba2924136e6bb3d0cce43
krlee <kiam125@gmail.com>
2016-05-29 22:16:12 +0800

    update commed

--- app/src/main/java/edu/fcu/pojeck/petlog_android/PetlogActivity.java
+++ app/src/main/java/edu/fcu/pojeck/petlog_android/PetlogActivity.java
@@ -3,7 +3,6 @@ package edu.fcu.pojeck.petlog_android;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.os.Bundle;
@@ -19,7 +18,6 @@ import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,31 +33,31 @@ public class PetlogActivity extends Activity {
     private int bmpW;// 動畫圖片寬度
     private View view1,view2,view3;//各個頁卡
     private LinearLayout speaker,home,settings;
+    private LinearLayout commends;
     private ArrayList PicturePath;//用来存放sd卡内的所有图片路径,在onCreate内使用
     private likeBase likeBase;
     private PictureBase pictureBase;
     private messageBase messageBase;
     private UserBase userBase;
-    Bitmap[] defaultPicture;//用于暂存预设图片
+    private View viewlay1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);//關閉狀態列
         setContentView(R.layout.petlogactivity);
+        InitInflaster();
         InitImageView();
         InitTextView();
         InitViewPager();
         InitLinearLayout();
-        PicturePath=PictureBase.getImagesFromSD();//现在Picture内存放着你放在SD卡目录内的所有图片路径(SD卡内文件夹内的不算)
-        likeBase=new likeBase();
-        pictureBase=new PictureBase();
-        messageBase=new messageBase();
-        userBase=new UserBase();
-       defaultPicture=new Bitmap[1];
-       defaultPicture[0]=BitmapFactory.decodeFile((String)PicturePath.get(0));
-        Toast.makeText(this,"find "+PicturePath.size()+" picture",Toast.LENGTH_LONG).show();
+        InitBase();
 
-        pictureBase.newapicture(defaultPicture,"this is my first task",0,"2016.5.28", messageBase,likeBase);
+    }
+
+    private void InitInflaster(){
+        LayoutInflater inflater = LayoutInflater.from(PetlogActivity.this);
+        viewlay1 = inflater.inflate(R.layout.lay1, null);
     }
 
     private void InitViewPager() {
@@ -95,10 +93,20 @@ public class PetlogActivity extends Activity {
         speaker = (LinearLayout)findViewById(R.id.speaker);
         home = (LinearLayout)findViewById(R.id.home);
         settings = (LinearLayout)findViewById(R.id.settings);
+        commends = (LinearLayout)viewlay1.findViewById(R.id.commends);
 
         speaker.setOnClickListener(MySpeakerListener);
         home.setOnClickListener(MyHomeListener);
         settings.setOnClickListener(MySettingListener);
+        //commends.setOnClickListener(MyCommendsListener);
+        commends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent();
+                intent.setClass(PetlogActivity.this,CommendsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     /**
      2      * 初始化动画
@@ -116,9 +124,15 @@ public class PetlogActivity extends Activity {
         imageView.setImageMatrix(matrix);// 設置動畫初始位置
     }
 
-    /**
-     *
-     * 头标点击监听 3 */
+    private void InitBase(){
+        likeBase=new likeBase();
+        pictureBase=new PictureBase();
+        messageBase=new messageBase();
+        userBase=new UserBase();
+        //Toast.makeText(this,"find "+PicturePath.size()+" picture",Toast.LENGTH_LONG).show();
+    }
+
+    // -------------------       設定監聽器       -------------------//
     private class MyOnClickListener implements View.OnClickListener {
         private int index=0;
         public MyOnClickListener(int i){
@@ -127,7 +141,6 @@ public class PetlogActivity extends Activity {
         public void onClick(View v) {
             viewPager.setCurrentItem(index);
         }
-
     }
 
     private View.OnClickListener MyHomeListener = new View.OnClickListener() {
@@ -157,6 +170,18 @@ public class PetlogActivity extends Activity {
         }
     };
 
+    private View.OnClickListener MyCommendsListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent= new Intent();
+            intent.setClass(PetlogActivity.this,CommendsActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    // -------------------       設定監聽器       -------------------//
+
+
     public class MyViewPagerAdapter extends PagerAdapter {
         private List<View> mListViews;
 
@@ -169,7 +194,6 @@ public class PetlogActivity extends Activity {
             container.removeView(mListViews.get(position));
         }
 
-
         @Override
         public Object instantiateItem(ViewGroup container, int position) {  //設計viewpager的地方
             container.addView(mListViews.get(position), 0);
@@ -178,11 +202,12 @@ public class PetlogActivity extends Activity {
             switch(position){
                 case 0:
                     Humanhead=(ImageView)findViewById(R.id.Humanhead);
-                    Humanhead.setImageBitmap(defaultPicture[0]);
+                    Humanhead.setImageBitmap(PictureBase.getdefaultbitmapfromSD());
+
                     break;
                 case 1:
                     Humanhead=(ImageView)findViewById(R.id.Humanhead);
-                    Humanhead.setImageBitmap(defaultPicture[0]);
+                    Humanhead.setImageBitmap(PictureBase.getdefaultbitmapfromSD());
                     break;
                 case 2:
 
