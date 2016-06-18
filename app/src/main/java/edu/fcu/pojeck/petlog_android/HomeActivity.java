80c5fed15b6c3a778cfe499500d149f7a04e10b4
yyaya1431tw <yyaya1431tw@yahoo.com.tw>
2016-06-18 21:02:33 +0800

    可上傳大頭貼 by yyaya1431tw

--- app/src/main/java/edu/fcu/pojeck/petlog_android/HomeActivity.java
+++ app/src/main/java/edu/fcu/pojeck/petlog_android/HomeActivity.java
@@ -1,17 +1,54 @@
 package edu.fcu.pojeck.petlog_android;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final int SELECTED_PIC=100;
+    ImageView iv;
+    Button bt1;
+    Uri ImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        iv=(ImageView)findViewById(R.id.headicon);
+        bt1=(Button)findViewById(R.id.changebt);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
     }
+
+    private void openGallery(){
+        Intent gallery= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, SELECTED_PIC);
+    }
+
+    protected void onActivityResult (int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        iv.setBackgroundColor(android.graphics.Color.parseColor("#00000000"));
+        if(resultCode == RESULT_OK && requestCode==SELECTED_PIC){
+            ImageUri = data.getData();
+            iv.setImageURI(ImageUri);
+        }
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,7 +72,5 @@ public class HomeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void test() {
-        
-    }
+
 }
